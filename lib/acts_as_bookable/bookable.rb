62fc17e1025a7f32e0b8f987832686a56c4f5e0b
Daniel Knight <daniel@knight.gb.net>
2025-08-07 17:48:25 +0100

    Define acts_as_bookable as a class macro using ClassMethods

--- lib/acts_as_bookable/bookable.rb
+++ lib/acts_as_bookable/bookable.rb
@@ -1,56 +1,55 @@
+# lib/acts_as_bookable/bookable.rb
 module ActsAsBookable
   module Bookable
+    def self.included(base)
+      base.extend ClassMethods
+    end
 
+    module ClassMethods
+      def acts_as_bookable(**opts)
+        bookable(**opts)
+      end
+
+      private
+
+      def bookable(**opts)
+        unless respond_to?(:booking_opts)
+          class_attribute :booking_opts
+        end
+
+        self.booking_opts = opts
+
+        return if bookable? # already configured
+
+        class_eval do
+          serialize :schedule, IceCube::Schedule
+
+          has_many :bookings, as: :bookable, dependent: :destroy, class_name: '::ActsAsBookable::Booking'
+
+          validates_presence_of :schedule, if: :schedule_required?
+          validates_presence_of :capacity, if: :capacity_required?
+          validates_numericality_of :capacity, if: :capacity_required?, only_integer: true, greater_than_or_equal_to: 0
+
+          def self.bookable?
+            true
+          end
+
+          def schedule_required?
+            booking_opts && booking_opts[:time_type] != :none
+          end
+
+          def capacity_required?
+            booking_opts && booking_opts[:capacity_type] != :none
+          end
+        end
+
+        include ActsAsBookable::Bookable::Core
+      end
+    end
+
+    # fallback for when the model hasn't been initialized as bookable
     def bookable?
       false
-    end
-
-    ##
-    # Make a model bookable
-    #
-    # Example:
-    #   class Room < ActiveRecord::Base
-    #     acts_as_bookable
-    #   end
-    def acts_as_bookable(**opts)
-      bookable(**opts)
-    end
-
-    private
-
-    # Make a model bookable
-    def bookable(**opts)
-      unless respond_to?(:booking_opts)
-        class_attribute :booking_opts
-      end
-
-      self.booking_opts = opts
-
-      return if bookable? # already configured
-
-      class_eval do
-        serialize :schedule, IceCube::Schedule
-
-        has_many :bookings, as: :bookable, dependent: :destroy, class_name: '::ActsAsBookable::Booking'
-
-        validates_presence_of :schedule, if: :schedule_required?
-        validates_presence_of :capacity, if: :capacity_required?
-        validates_numericality_of :capacity, if: :capacity_required?, only_integer: true, greater_than_or_equal_to: 0
-
-        def self.bookable?
-          true
-        end
-
-        def schedule_required?
-          booking_opts && booking_opts[:time_type] != :none
-        end
-
-        def capacity_required?
-          booking_opts && booking_opts[:capacity_type] != :none
-        end
-      end
-
-      include Core
     end
   end
 end
